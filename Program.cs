1dcd215a05f3856469b64a8a35a068bb9d2e214a
joesRepos <joemitchell1717@gmail.com>
2026-03-30 17:28:40 +0100

    Set  up  boards.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void InitialiseBoards(char[,] hidden, char[,] visible)
+{
+    for (int r = 0; r < 5; r++)
+    {
+        for (int c = 0; c < 5; c++)
+        {
+            hidden[r, c] = '.';
+            visible[r, c] = '#';
+        }
+    }
+}
+
+
+char[,] hiddenBoard = new char[5, 5];
+char[,] visibleBoard = new char[5, 5];
+
+InitialiseBoards(hiddenBoard, visibleBoard);
